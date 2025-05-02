ec82f344d214339f5555185728f1b6b9f3e2d710
pateltaanish <67441358+pateltaanish@users.noreply.github.com>
2025-05-01 19:08:03 -0700

    Daily Whale (Optimized).sql

--- Daily Whale (Optimized).sql
+++ Daily Whale (Optimized).sql
@@ -1,17 +1,16 @@
 #Shows all BTC transactions happening with more than 1000BTC in a single day
 
-SELECT 
-  DATE(block_timestamp) AS transaction_date,
-  COUNT(*) AS whale_transactions
-FROM 
-  `bigquery-public-data.crypto_bitcoin.transactions`
-WHERE 
+SELECT
+ DATE(block_timestamp) AS transaction_date,
+ COUNT(1) AS whale_transactions
+FROM
+ `bigquery-public-data.crypto_bitcoin.transactions`
+WHERE
 # greater than 1000 BTC - 1 Bitcoin = 100,000,000 satoshis
-  output_value > 100000000000  
-GROUP BY 
-  transaction_date
-HAVING 
-  whale_transactions > 0
-ORDER BY 
-  whale_transactions DESC
+ output_value > 1e11 
+ AND block_timestamp IS NOT NULL  
+GROUP BY
+ transaction_date
+ORDER BY
+ whale_transactions DESC
 LIMIT 100;
