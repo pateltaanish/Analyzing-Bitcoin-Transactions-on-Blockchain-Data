0f74578427f4a69661ef3f38339445e974a220b7
pateltaanish <67441358+pateltaanish@users.noreply.github.com>
2025-05-01 20:10:31 -0700

    top 10 BTC transactions(Optimized).sql

--- top 10 BTC transactions(Optimized).sql
+++ top 10 BTC transactions(Optimized).sql
@@ -1,5 +1,12 @@
 #Query shows the top 10 largest BTC transactions
-SELECT 'bigquery-public-data.crypto_bitcoin.transactions.hash' AS UNIQUEID, block_timestamp,output_value 
-FROM `bigquery-public-data.crypto_bitcoin.transactions` 
-ORDER BY output_value DESC
+SELECT
+ 'bigquery-public-data.crypto_bitcoin.transactions.hash' AS UNIQUEID,
+ block_timestamp,
+ output_value
+FROM
+ `bigquery-public-data.crypto_bitcoin.transactions`
+WHERE
+ block_timestamp_month BETWEEN '2011-01-01' AND '2024-01-01'
+ORDER BY
+ output_value DESC
 LIMIT 10;
